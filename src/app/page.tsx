1320673b3682220bfc7f760fb8e939dd1c0350f6
Manuel Reichel <manuelreichel@gmail.com>
2024-03-18 17:54:48 -0300

    Fix styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-start p-24">
       <div className="mb-4">
-        <Link href="/posts/create" className="bg-white px-4 py-2 rounded">Create Post</Link>
+        <Link href="/posts/create" className="bg-slate-400 px-4 py-2 rounded">Create Post</Link>
       </div>
       <div className="mb-32 grid gap-x-8 gap-y-4 text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         {posts.map(post => {
